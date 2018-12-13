3cc56a51304b87332fae4fe7b493ffdc608f1ab6
Kyle Jennings <kjennings.dev@gmail.com>
2018-12-13 12:47:38 -0600

    Adds cpu-average-usage component

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export { LineGraph } from './src/components/line-graph';
 
 export { CpuMaxUsage } from './src/components/cpu/cpu-max-usage/cpu-max-usage';
 export { SystemUsage } from './src/components/cpu/system-usage/system-usage';
+export { AverageUsage } from './src/components/cpu/average-usage/average-usage';

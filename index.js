716695af2e7945f20696897e680a622bee2fd623
Kyle Jennings <kjennings.dev@gmail.com>
2018-12-13 13:08:48 -0600

    Adds cpu-wait-average component

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ export { LineGraph } from './src/components/line-graph';
 export { CpuMaxUsage } from './src/components/cpu/cpu-max-usage/cpu-max-usage';
 export { SystemUsage } from './src/components/cpu/system-usage/system-usage';
 export { AverageUsage } from './src/components/cpu/average-usage/average-usage';
+export { WaitAverage } from './src/components/cpu/wait-average/wait-average';
---
+++ src/components/cpu/wait-average/wait-average.js
@@ -0,0 +1,43 @@
+import { Defaults } from "../../defaults";
+
+/**
+ * @name WaitAverage
+ * @description
+ * @extends HTMLElement
+ * Graph representing System Usage information
+ */
+export class WaitAverage extends HTMLElement {
+ 
+    constructor() {
+        super();
+    }
+
+    /**
+     * @name connectedCallback
+     * @description
+     * Call back for when the component is attached to the DOM
+     */
+    connectedCallback() {
+        var defaults = new Defaults();
+        var margin = this.dataset.margin || defaults.margin;
+        var height = (this.dataset.height || defaults.height) - margin.top - margin.bottom;
+        var width = (this.dataset.width || defaults.width) - margin.left - margin.right;
+        var lineColor = this.dataset.lineColor || defaults.lineColor;
+
+        this.innerHTML = "<line-graph data-margin=" + JSON.stringify(margin) + 
+            " data-height=" + height + 
+            " data-width=" + width + 
+            " data-graph=" + this.dataset.graph + 
+            " data-line-color=" + lineColor + 
+            "></lineGraph>";
+    }
+
+    /**
+     * @name disconnectedCallback
+     * @description
+     * Call back for when the component is detached from the DOM
+     */
+    disconnectedCallback() {}
+}
+
+customElements.define('cpu-wait-average', WaitAverage);

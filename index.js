130c48202540f2678c05711ed81fb0f2c1d12885
Kyle Jennings <kjennings.dev@gmail.com>
2018-12-13 14:30:25 -0600

    Adds cpu-irq-average component

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ export { CpuMaxUsage } from './src/components/cpu/cpu-max-usage/cpu-max-usage';
 export { SystemUsage } from './src/components/cpu/system-usage/system-usage';
 export { AverageUsage } from './src/components/cpu/average-usage/average-usage';
 export { WaitAverage } from './src/components/cpu/wait-average/wait-average';
+export { IrqAverage } from './src/components/cpu/irq-average/irq-average';

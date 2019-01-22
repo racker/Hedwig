0f6954854fa04787c5a90a6e9d916d4f222f5348
FarajiA <sacbeme04@yahoo.com>
2019-01-22 15:10:04 -0600

    server.js file

---
+++ bin/server.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict';
+const CONFIG = require('../_config');
+const express = require('express');
+
+// Create Express App
+const app = express();
+app.use('/hedwig', express.static(CONFIG.srcComponents));
+app.use('/', express.static(CONFIG.srcComponents));
+app.use('/dist', express.static(CONFIG.distDir));
+
+let server = {};
+let appServer = "";
+server.start = () => {
+    appServer = app.listen(CONFIG.port, () => {
+        console.log(`🚀 serving at ${CONFIG.host}:${CONFIG.port}`);
+    });
+};
+
+server.stop = () => {
+    appServer.close(function() {
+        console.log(`server stopped 🛑 on ${CONFIG.host}:${CONFIG.port}`);
+        process.exit();
+    });
+};
+
+module.exports = server;

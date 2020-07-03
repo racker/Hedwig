e209379c7ee866cefbb0cc87d153d8ea19c9c846
prashant mishra <prashant.mishra@rackspace.com>
2020-07-03 18:46:23 +0530

    multiline mock added

--- src/line-graph.js
+++ src/line-graph.js
@@ -1,13 +1,13 @@
-
 import Highcharts from 'highcharts/highcharts';
-import { AxisLeft } from './helpers/axisConverter';
+import {
+  AxisLeft
+} from './helpers/axisConverter';
 
 export class LineGraph extends HTMLElement {
-
-    constructor() {
-        super();
-      }
-      /**
+  constructor() {
+    super();
+  }
+  /**
    * @name parseData
    * @param {Array} data
    * @description
@@ -31,114 +31,126 @@ export class LineGraph extends HTMLElement {
       return {
         group,
         datapoints: data.filter(d => d[grouping] === group).map((d) => {
-          return { time: new Date(d.time), value: +d.mean }
+          return {
+            time: new Date(d.time),
+            value: +d.mean
+          }
         })
       }
     });
   }
-    connectedCallback() {
-      this.innerHTML = `<div style="height: 400px,width:400px" id="container"></div>`;
-      var container = document.querySelector(`#container`);
-      Highcharts.chart("container", this.optionObject());
-      this.attachShadow({
-        mode: 'open'
-      });
-      this.shadowRoot.appendChild(container);
-    }
-
-    series(data){
-      let jsonObj=JSON.parse(data);
-      let grouping=this.parseData(jsonObj);
-      let series=[]
-      grouping.forEach(e =>{
-      series.push(this.sereisData(e.group,e.datapoints));  
-      })      
-      return series;        
-    }
-
-    sereisData(name,datapoints){
-      let arr=[];
-      datapoints.forEach(e =>{
-            arr.push([new Date(e.time).getTime(),e.value])
-        })
-        return {
-          name,
-          data:arr,
-        };
-    }
-  
-    optionObject(){
-
-        return {chart: {
-            height: 300,
-            width:600,
-            type: 'line'
-        },
-        // colors:this.getAttribute('data-line-color')?[this.getAttribute('data-line-color'),Highcharts.getOptions().colors]:Highcharts.getOptions().colors,
-        unit:{
-          value:this.getAttribute('data-unit')
-        },
-        credits: {
-          enabled: false
-        },
-        title: {
-            text: ''
-        },
-        subtitle: {
-            text: ''
-        },
-        legend: {
-          layout: 'horizontal', // default
-      },
-        yAxis: {            
-            labels: {
-                formatter: function () {
-                  let u=new AxisLeft();
-                    return  this.axis.defaultLabelFormatter.call(this)+ u.convert(this.chart.options.unit.value);
-                }            
-            }
-        },
-        xAxis: {
-            type: 'datetime',
-            dateTimeLabelFormats: {
-                second: '%H:%M:%S',
-                // minute: '%H:%M',
-                // hour: '%H:%M',
-                // day: '%b. %e',
-                // week: '%b. %e',
-                // month: '%b. %y',
-                // year: '%Y'
-              }
-            
-        },
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-            }
-        },
-        
-        series: this.series(this.getAttribute('data-graph')),
-    
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 200
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-        }
-    
-    }
-    }
-  
+  connectedCallback() {
+    this.innerHTML = `<div style="height: 400px,width:400px" id="container"></div>`;
+    var container = document.querySelector(`#container`);
+    Highcharts.chart("container", this.optionObject());
+    this.attachShadow({
+      mode: 'open'
+    });
+    this.shadowRoot.appendChild(container);
   }
-      
-  customElements.define('line-graph', LineGraph);
+
+  /**
+   * make series object
+   * @param {stringyfy data} data 
+   * @returns Array of objects
+   */
+  series(data) {
+    let jsonObj = JSON.parse(data);
+    let grouping = this.parseData(jsonObj);
+    let series = []
+    grouping.forEach(e => {
+      series.push(this.sereisData(e.group, e.datapoints));
+    })
+    return series;
+  }
+
+  /**
+   * 
+   * @param {name for series} name 
+   * @param {datapoints with time and value} datapoints 
+   */
+  sereisData(name, datapoints) {
+    let arr = [];
+    datapoints.forEach(e => {
+      arr.push([new Date(e.time).getTime(), e.value])
+    })
+    return {
+      name,
+      data: arr,
+    };
+  }
+
+  /**
+   * Highchart option object
+   */
+  optionObject() {
+    return {
+      chart: {
+        height: 300,
+        width: 600,
+        type: 'line'
+      },
+      // colors:this.getAttribute('data-line-color')?[this.getAttribute('data-line-color'),Highcharts.getOptions().colors]:Highcharts.getOptions().colors,
+      unit: {
+        value: this.getAttribute('data-unit')
+      },
+      // remove highchart water mark
+      credits: {
+        enabled: false
+      },
+      legend: {
+        layout: 'horizontal', // default
+      },
+      yAxis: {
+        labels: {
+          // y axis labels formatting
+          formatter: function () {
+            let u = new AxisLeft();
+            return this.axis.defaultLabelFormatter.call(this) + u.convert(this.chart.options.unit.value);
+          }
+        }
+      },
+      xAxis: {
+        type: 'datetime',
+        dateTimeLabelFormats: {
+          second: '%H:%M:%S',
+          // minute: '%H:%M',
+          // hour: '%H:%M',
+          // day: '%b. %e',
+          // week: '%b. %e',
+          // month: '%b. %y',
+          // year: '%Y'
+        }
+
+      },
+      plotOptions: {
+        series: {
+          label: {
+            connectorAllowed: false
+          },
+        }
+      },
+
+      series: this.series(this.getAttribute('data-graph')),
+
+      responsive: {
+        rules: [{
+          condition: {
+            maxWidth: 200
+          },
+          chartOptions: {
+            legend: {
+              layout: 'horizontal',
+              align: 'center',
+              verticalAlign: 'bottom'
+            }
+          }
+        }]
+      }
+
+    }
+  }
+
+}
+
+customElements.define('line-graph', LineGraph);

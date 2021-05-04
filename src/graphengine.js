1542d6e65f4752700d16f40488e4aa51d46775a8
FarajiA <sacbeme04@yahoo.com>
2021-05-03 23:03:28 -0500

    HOTFIX: add condition to render

--- src/graphengine.js
+++ src/graphengine.js
@@ -10,7 +10,7 @@ import { FindInfo } from "./helpers/supportedInfo";
 export class GraphEngine extends HTMLElement {
     constructor() {
         super();
-       
+
     }
 
     /**
@@ -19,7 +19,7 @@ export class GraphEngine extends HTMLElement {
      * Call back for when the component is attached to the DOM
      */
     connectedCallback() {
-       
+
         this.defaults = {};
         var defaults = new Defaults();
 
@@ -39,7 +39,7 @@ export class GraphEngine extends HTMLElement {
      * @param {string} data this param is collected from the data-graph attribute
      */
     render () {
-        if (this.defaults) {
+        if (this.defaults && this.graphData) {
             this.innerHTML = "<line-graph data-margin=" + JSON.stringify(this.defaults.margin) +
             " data-height=" + this.defaults.height +
             " data-width=" + this.defaults.width +
@@ -58,7 +58,6 @@ export class GraphEngine extends HTMLElement {
      * @param {string} data This param is stringified JSON data setting
      */
     dataPoints(data){
-       
         this.graphInfo = new FindInfo().info(this.dataset.type, this.dataset.field);
         this.graphData = data;
     }
@@ -69,7 +68,7 @@ export class GraphEngine extends HTMLElement {
      * Call back for when the component is detached from the DOM
      */
     disconnectedCallback() {
-        
+
     }
 
     /**
